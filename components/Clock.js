b23fbca98bbe0eaaff0061a6b849b2699a5aeb73
ting-wei <inkcarve@gmail.com>
2018-11-12 18:25:26 +0800

    renew

--- components/Clock.js
+++ components/Clock.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
-export default function (props) {
+export default (function (props) {
     return (React.createElement("div", { className: props.light ? 'light' : '' }, format(new Date(props.lastUpdate))));
-};
+});
 var format = function (t) { return pad(t.getUTCHours()) + ":" + pad(t.getUTCMinutes()) + ":" + pad(t.getUTCSeconds()); };
 var pad = function (n) { return n < 10 ? "0" + n : n; };
 //# sourceMappingURL=Clock.js.map
