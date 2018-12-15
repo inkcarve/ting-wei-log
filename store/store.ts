c24f9ae24cd5007596f55a8bac6fa8490c95d8ca
tingwei <inkcarve@gmail.com>
2018-12-15 14:01:22 +0800

    update tslint-react

--- store/store.ts
+++ store/store.ts
@@ -1,35 +1,35 @@
-import { action, observable } from 'mobx'
-import authorData from '../data/author-data';
-let store:any ;
+import { action, observable } from "mobx";
+import authorData from "../data/author-data";
+let store: any;
 
 class Store {
-  @observable lastUpdate = 0
-  @observable light = false
+  @observable lastUpdate = 0;
+  @observable light = false;
 
-  private timer:any;
-  public authorData:any;
-  constructor (isServer:boolean, lastUpdate:any) {
-    this.lastUpdate = lastUpdate
-    this.authorData = authorData
+  private timer: any;
+  public authorData: any;
+  constructor(isServer: boolean, lastUpdate: any) {
+    this.lastUpdate = lastUpdate;
+    this.authorData = authorData;
   }
 
   @action start = () => {
     this.timer = setInterval(() => {
-      this.lastUpdate = Date.now()
-      this.light = true
-    })
-  }
+      this.lastUpdate = Date.now();
+      this.light = true;
+    });
+  };
 
-  stop = () => clearInterval(this.timer)
+  stop = () => clearInterval(this.timer);
 }
 
-export function initStore (isServer:boolean, lastUpdate = Date.now()) {
+export function initStore(isServer: boolean, lastUpdate = Date.now()) {
   if (isServer) {
-    return new Store(isServer, lastUpdate)
+    return new Store(isServer, lastUpdate);
   } else {
     if (!store) {
-      store = new Store(isServer, lastUpdate)
+      store = new Store(isServer, lastUpdate);
     }
-    return store
+    return store;
   }
 }
