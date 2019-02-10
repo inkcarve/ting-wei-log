38f75870915834c3dd7f3d1c1d40ab4c1139ed9e
tingwei <inkcarve@gmail.com>
2019-02-10 23:50:39 +0800

    add index VR button

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,11 +29,13 @@ class PageIndex extends React.Component<any, any> {
               <div className="col-12 p-0 position-relative">
                 <div className="embed-responsive embed-responsive-16by9 position-relative index-bg-box d-flex align-items-center">
                   <div className="d-none d-md-block col-md-5 text-right">
+                  <a className="btn btn-outline-light mr-4 px-md-5 py-md-4 mb-3 col-md-12 col-lg-10 col-xl-8" href="/vr"><h3>VR</h3></a>
                   <a className="btn btn-outline-light mr-4 px-md-5 py-md-4 mb-3 col-md-12 col-lg-10 col-xl-8" href="/redux-game"><h3>{this.props.t('reduxGame')}</h3></a>
                   <a className="btn btn-outline-light mr-4 px-md-5 py-md-4 col-md-12 col-lg-10 col-xl-8" href="/resume"><h3>{this.props.t('author')}</h3></a>
                   </div>
                 </div>
                 <div className="d-md-none text-center my-5">
+                <a className="btn btn-outline-primary px-2 py-2 mb-3 col-8" href="/vr"><h4>VR</h4></a>
                   <a className="btn btn-outline-primary px-2 py-2 mb-3 col-8" href="/redux-game"><h4>The Shining Cat</h4></a>
                   <a className="btn btn-outline-primary px-2 py-2 col-8" href="/resume"><h4>More about Me</h4></a>
                   </div>
